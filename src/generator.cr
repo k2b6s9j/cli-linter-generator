2a6883f276dfd07537cc8b2a90a628c4e49c2600
Kepler Sticka-Jones <kepler+git@stickajones.org>
2015-07-17 10:16:06 -0700

    And at long last the source code.

---
+++ src/generator.cr
@@ -0,0 +1,134 @@
+require "ecr/macros"
+require "colorize"
+
+module Linter
+  module Generator
+    TEMPLATE_DIR = "#{__DIR__}/template"
+
+    class Config
+      property name
+      property version
+      property command
+      property scopes
+      property regex
+      property owner
+      property silent
+
+      def initialize
+        @name = "linter-echo"
+        @version = "0.0.0"
+        @command = "echo"
+        @scopes = "*"
+        @regex = "(.+)"
+        @owner = "AtomLinter"
+        @silent = false
+      end
+
+      macro write_without_newline(property_name)
+        def {{property_name.id}}=({{property_name.id}})
+          @{{property_name.id}} = {{property_name.id}}.to_s.chomp("\n")
+        end
+      end
+
+      write_without_newline name
+      write_without_newline version
+      write_without_newline command
+      write_without_newline scopes
+      write_without_newline regex
+      write_without_newline owner
+
+      def scopes
+        scopes = @scopes.to_s.split(", ")
+      end
+
+      def arguments
+        args = @command.to_s.split(" ")
+        args - [args[0]]
+      end
+    end
+
+    abstract class View
+      getter config
+
+      @@views = [] of View.class
+
+      def self.views
+        @@views
+      end
+
+      def self.register(view)
+        views << view
+      end
+
+      def initialize(@config)
+      end
+
+      def render
+        Dir.mkdir_p(File.dirname(full_path))
+        File.write(full_path, to_s)
+        puts log_message unless config.silent
+      end
+
+      def log_message
+        "      #{"create".colorize(:light_green)}  #{full_path}"
+      end
+
+      abstract def full_path
+    end
+
+    class InitProject
+      getter config
+
+      def initialize(@config)
+      end
+
+      def run
+        views.each do |view|
+          view.new(config).render
+        end
+      end
+
+      def views
+        View.views
+      end
+    end
+
+    macro template(name, template_path, full_path)
+      class {{name.id}} < View
+        ecr_file "{{TEMPLATE_DIR.id}}/{{template_path.id}}"
+        def full_path
+          "output/#{{{full_path}}}"
+        end
+      end
+
+      View.register({{name.id}})
+    end
+
+    template MainJavaScriptView, "main.js.ecr", "lib/main.js"
+    template ProjectJsonView, "project.json.ecr", "project.json"
+  end
+end
+
+config = Linter::Generator::Config.new
+
+puts "Linter Name:".colorize.bold
+config.name = gets
+puts "Linter Version:".colorize.bold
+config.version = gets
+puts "Linter Command:".colorize.bold
+config.command = gets
+puts "Linter Scopes:".colorize.bold
+config.scopes = gets
+puts "Linter Regex:".colorize.bold
+config.regex = gets
+puts "Linter Repo Owner:".colorize.bold
+config.owner = gets
+
+puts ""
+puts ""
+
+puts "Beginning Creation of #{config.name}".colorize(:green).bold
+
+Linter::Generator::InitProject.new(config).run
+
+puts "Creation of #{config.name} Finished".colorize(:green).bold
